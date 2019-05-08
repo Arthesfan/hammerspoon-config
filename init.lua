9c7cfac2a3640a3f8804a06b398135abf0ad3c50
Arthesfan <1634440404@qq.com>
2019-05-08 17:36:11 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,14 @@
 print('==================================================')
-require "headphone.headphone"
+--require "headphone.headphone"
 require "hotkey.hotkey"
-require "ime.ime"
+--require "ime.ime"
 require "reload.reload"
 require "usb.usb"
-require "wifi.wifi"
+--require "wifi.wifi"
 require "window.window"
 require "clipboard.clipboard"
 require "statuslets.statuslets"
-require "volume.volume"
+--require "volume.volume"
 require "weather.weather"
-require "speaker.speaker"
+--require "speaker.speaker"
 
--- Private use
-if (hs.host.localizedName() == 'kaboom的MacBook Pro') then
-	require("autoscript.autoscript")
-end
